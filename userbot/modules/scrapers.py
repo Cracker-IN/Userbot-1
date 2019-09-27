634b8663a24a21ec40dd5e02811ffd1fec63a593
Cracker-IN <55758730+Cracker-IN@users.noreply.github.com>
2019-09-27 23:09:31 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -87,7 +87,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="[Spyder](https://t.me/spyderzzz/) iz Gawd + peru af.",
+         caption="[Cʀᴀᴄᴋᴇʀ™♠️](https://t.me/Cracker_IN/) iz Gawd + peru af.",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
